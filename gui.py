99e253e0aaa35208ccfa6958bc8254a005bef3de
HDThang <huynhthang1108@gmail.com>
2025-03-26 10:47:18 +0700

    change print answer logic + add markdonw to measure the answertime

--- gui.py
+++ gui.py
@@ -16,27 +16,28 @@ history = [
 ]
 
 
-def response_message(message, history):
-    # remove media files from the message
-    history.append({"role": "user", "content": message['text']})
-    # history.append(ChatMessage(role="user", content=message))
+def add_message(message, history):
+    history.append(ChatMessage(role="user", content=message['text']))
+    return "", history
+
+
+def response_message(history):
+    start_time = time.time()
     response = utils.answer_message(model, tokenizer, history)
-    history.append({"role": "assistant", "content": response})
-    # history.append(ChatMessage(role="assistant", content=response))
-    return response, history
+    ans_time = time.time() - start_time
+    history.append(ChatMessage(role="assistant", content=response))
 
-
-def bot_responser(history):
-    output_response = ''
-    if history[-1]['role'] == 'assistant':
-        for character in history[-1]['content']:
-            output_response += character
-            time.sleep(0.3)
-            yield output_response
+    return history, f"Last answer response time: {ans_time:.3f}s"
 
 
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot(type="messages")
+    chatbot = gr.Chatbot(
+        type="messages",
+        height=800,
+        max_height=900,
+        min_height=600,
+    )
+    ans_time_output = gr.Markdown("Last answer response time: 0s")
     chat_input = gr.MultimodalTextbox(
         interactive=True,
         file_count="multiple",
@@ -45,10 +46,11 @@ with gr.Blocks() as demo:
         sources=["microphone", "upload"],
     )
     clear = gr.Button("Clear")
-    chat_msg = chat_input.submit(response_message, [chat_input, chatbot],
+    chat_msg = chat_input.submit(add_message, [chat_input, chatbot],
                                  [chat_input, chatbot])
-    chat_msg.then(lambda: gr.MultimodalTextbox(interactive=True), chat_msg,
-                  [chat_input])  # clear the chat input bar
+    chat_msg.then(response_message,
+                  inputs=chatbot,
+                  outputs=[chatbot, ans_time_output])
     clear.click(lambda: None, None, chatbot)
 
 if __name__ == "__main__":
